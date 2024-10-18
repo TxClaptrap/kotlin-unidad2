3ab884a0f150de2cccc6d080a007ef60e4164ca2
TxClaptrap <150007748+TxClaptrap@users.noreply.github.com>
2024-10-18 14:24:00 +0200

    Insertar imágenes e iconos

--- app/src/main/java/com/example/unidad2/ui/pruebas/TextScreen.kt
+++ app/src/main/java/com/example/unidad2/ui/pruebas/TextScreen.kt
@@ -1,15 +1,25 @@
 package com.example.unidad2.ui.pruebas
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.unidad2.R
 
 
 @Composable
@@ -23,7 +33,7 @@ fun Formulario(apellido:String, onApellidoChange:(String) -> Unit) {
 }
 
 
-//@Preview(showBackground = true)
+
 @Composable
 fun PantallaPrincipal(viewModel: TextViewModel) {
     val apellido: String by viewModel.apellido.observeAsState("")
@@ -33,4 +43,30 @@ fun PantallaPrincipal(viewModel: TextViewModel) {
 
     }
 }
+
+@Composable
+
+fun ImagenPreview() {
+    PantallaPrincipal(TextViewModel())
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PruebaImagen() {
+    Column (modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = ""
+        )
+        Icon(
+            imageVector = Icons.Outlined.Favorite,
+            contentDescription = ""
+        )
+
+    }
+
+}
+
+
+
 
